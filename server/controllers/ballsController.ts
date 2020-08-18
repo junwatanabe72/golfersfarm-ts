701ab1c0b2b8b1c500d49f6ecfeaeb30e59d0764
jun_mac <junwatanabe72@gmail.com>
2020-08-18 16:06:59 +0900

    balls

--- server/controllers/ballsController.ts
+++ server/controllers/ballsController.ts
@@ -1,36 +1,36 @@
 import { Request, Response, NextFunction } from "express";
-import db from "../models"
+import db from "../models";
 
 const balls = db.Ball;
 const makers = db.Maker;
 
 export default {
   async show(req: Request, res: Response, next: NextFunction) {
-    try{
-    const targetBall = await balls.findOne({
-      where: { userId: req.params.id},
-      raw: false,
-      include: [
-            {
-              model: makers,
-              required: false,
-            },
-          ],
-    })
-      res.json({ targetBall});
+    try {
+      const targetBall = await balls.findOne({
+        where: { userId: req.params.id },
+        raw: false,
+        include: [
+          {
+            model: makers,
+            required: false,
+          },
+        ],
+      });
+      res.json({ targetBall });
     } catch (error) {
-      res.status(400)
-      return next(error)
+      res.status(400);
+      return next(error);
     }
   },
   async update(req: Request, res: Response, next: NextFunction) {
     const { ball } = req.body;
     try {
-      const updateBall = await balls.updateBall(req.params.id,ball);
+      const updateBall = await balls.updateBall(req.params.id, ball);
       res.status(201).json({ updateBall });
     } catch (error) {
-      res.status(404)
-      return next(error)
+      res.status(404);
+      return next(error);
     }
   },
 
